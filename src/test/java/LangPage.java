2ab08aadf6cc07d98b5e85ef5676fe0213c7cb59
Sergey Obidin <player181285@gmail.com>
2022-12-26 14:21:58 +0400

    new config

--- src/test/java/LangPage.java
+++ src/test/java/LangPage.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Selenide.$x;
 public class LangPage {
 
     private final SelenideElement langEn = $x("//a [@href='https://capital.com/']");
-    private final SelenideElement english = $x("//i[@class='cc-flags cc-flags--gb']/..");
+    private final SelenideElement english = $x("//i[@class='cc-flags cc-flags--gb']//..");
     private final SelenideElement arab = $x("//i[@class='cc-flags cc-flags--ab']/..");
     private final SelenideElement id = $x("//i[@class='cc-flags cc-flags--id']/..");
     private final SelenideElement bg = $x("//i[@class='cc-flags cc-flags--bg']/..");
@@ -39,94 +39,6 @@ public class LangPage {
     private final SelenideElement th = $x("//i[@class='cc-flags cc-flags--th']/..");
     private final SelenideElement zn = $x("//i[@class='cc-flags cc-flags--zn']/..");
     private final SelenideElement cn = $x("//i[@class='cc-flags cc-flags--cn']/..");
-//    public SelenideElement getBg() {
-//        return bg;
-//    }
-//    public SelenideElement getCz() {
-//        return cz;
-//    }
-//    public SelenideElement getDk() {
-//        return dk;
-//    }
-//    public SelenideElement getDe() {
-//        return de;
-//    }
-//    public SelenideElement getEe() {
-//        return ee;
-//    }
-//    public SelenideElement getGr() {
-//        return gr;
-//    }
-//    public SelenideElement getEs() {
-//        return es;
-//    }
-//    public SelenideElement getFr() {
-//        return fr;
-//    }
-//    public SelenideElement getHr() {
-//        return hr;
-//    }
-//    public SelenideElement getId() {
-//        return id;
-//    }
-//    public SelenideElement getArab() {
-//        return arab;
-//    }
-//    public SelenideElement getEnglish() {
-//        return english;
-//    }
-//    public SelenideElement getIt() {
-//        return it;
-//    }
-//    public SelenideElement getLv() {
-//        return lv;
-//    }
-//    public SelenideElement getLt() {
-//        return lt;
-//    }
-//    public SelenideElement getHu() {
-//        return hu;
-//    }
-//    public SelenideElement getNl() {
-//        return nl;
-//    }
-//    public SelenideElement getPl() { return pl; }
-//    public SelenideElement getPt() {
-//        return pt;
-//    }
-//    public SelenideElement getRo() {
-//        return ro;
-//    }
-//    public SelenideElement getRu() {
-//        return ru;
-//    }
-//    public SelenideElement getSk() {
-//        return sk;
-//    }
-//    public SelenideElement getSi() {
-//        return si;
-//    }
-//    public SelenideElement getFi() {
-//        return fi;
-//    }
-//    public SelenideElement getSe() {
-//        return se;
-//    }
-//    public SelenideElement getVn() {
-//        return vn;
-//    }
-//    public SelenideElement getTh() {
-//        return th;
-//    }
-//    public SelenideElement getZn() {
-//        return zn;
-//    }
-//    public SelenideElement getCn() {
-//        return cn;
-//    }
-//     public SelenideElement getLangEn() {
-//        return langEn;
-//    }
     public void choseLanguage(SelenideElement language){
         language.click();
     }
